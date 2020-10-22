e2ef9a115d76b83a31146859be17ba3ef47c647c
ryan-wales <ryan@Ryans-MacBook-Pro.local>
2020-10-22 23:50:17 +0100

    More learnings :) 2020-10-22

--- beginner/classesobjects.py
+++ beginner/classesobjects.py
@@ -1,5 +1,6 @@
 from Student import Student
 
 student1 = Student("John Jones","Physics",3.1,False)
+student2 = Student("John Jones","Physics",5.5,False)
 
-print(student1.name)
+print(student2.on_honor_roll())
